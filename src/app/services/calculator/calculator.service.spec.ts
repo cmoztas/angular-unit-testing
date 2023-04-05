aa2b4660bba75550964550d3b93ba8360b99b6c9
cagkanoztas <cagkan.oztas@jengal.com>
2023-04-05 13:04:25 +0300

    Using BeforeEach setup method to structure the Angular Test Cases for avoiding Repetitive code

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,17 +1,21 @@
 import {CalculatorService} from './calculator.service';
 
 describe('Calculator Service', (): void => {
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+
+  beforeEach((): void => {
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  })
+
   it('should add two numbers', (): void => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator: CalculatorService = new CalculatorService(mockLoggerService);
     let result: number = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', (): void => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator: CalculatorService = new CalculatorService(mockLoggerService);
     let result: number = calculator.subtract(2, 2);
     expect(result).toBe(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
