630f0a8feca19eabf3d5068df181307942952898
cagkanoztas <cagkan.oztas@jengal.com>
2023-04-07 13:08:59 +0300

    Add HttpClientTestingModule to Post Service for Resolving HttpClient Dependency

---
+++ src/app/services/post/post-http-service.spec.ts
@@ -0,0 +1,36 @@
+import {PostService} from './post.service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController, TestRequest} from '@angular/common/http/testing';
+import {Post} from '../../models/post.model';
+
+describe('postService (HttpClientTestingModule)', (): void => {
+  let postService: PostService;
+  let httpTestingController: HttpTestingController;
+  const POSTS: Post[] = [
+    {id: 1, userId: 1, title: 'title 1', body: 'body 1'},
+    {id: 2, userId: 1, title: 'title 2', body: 'body 2'},
+    {id: 3, userId: 1, title: 'title 3', body: 'body 3'}
+  ];
+
+  beforeEach((): void => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PostService],
+    });
+
+    postService = TestBed.inject(PostService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  describe('getPosts()', (): void => {
+    it('should return posts when getPosts() is called', (done: DoneFn): void => {
+      postService.getPosts().subscribe((data: Post[]): void => {
+        expect(data).toEqual(POSTS);
+        done();
+      });
+      const request: TestRequest = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/posts');
+      request.flush(POSTS);
+      expect(request.request.method).toBe('GET');
+    });
+  });
+});
