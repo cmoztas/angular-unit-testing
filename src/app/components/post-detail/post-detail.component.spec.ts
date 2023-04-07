84b397accab1f1625790ed2ee59bf589d6c2a576
cagkanoztas <cagkan.oztas@jengal.com>
2023-04-08 00:04:54 +0300

    Testing Post Detail Component Template using nativeElement & debugElement

--- src/app/components/post-detail/post-detail.component.spec.ts
+++ src/app/components/post-detail/post-detail.component.spec.ts
@@ -3,32 +3,52 @@ import {PostDetailComponent} from './post-detail.component';
 import {Location} from '@angular/common';
 import {PostService} from '../../services/post/post.service';
 import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
+import {Post} from '../../models/post.model';
+import {By} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 describe('Post Detail Component', (): void => {
   let fixture: ComponentFixture<PostDetailComponent>;
+  let mockPostService: jasmine.SpyObj<PostService>;
 
-  let mockLocation = jasmine.createSpyObj(['back']);
-  let mockPostService = jasmine.createSpyObj(['getPost', 'updatePost']);
-  let mockActivatedRoute = {
-    snapshot: {
-      paramMap: {
-        get: (): string => {
-          return '3';
+  beforeEach((): void => {
+    let mockLocation = jasmine.createSpyObj(['back']);
+    mockPostService = jasmine.createSpyObj(['getPost', 'updatePost']);
+    let mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: (): string => {
+            return '3';
+          }
         }
       }
     }
-  }
 
-  beforeEach((): void => {
     TestBed.configureTestingModule({
       declarations: [PostDetailComponent],
       providers: [
         {provide: Location, useValue: mockLocation},
         {provide: PostService, useValue: mockPostService},
         {provide: ActivatedRoute, useValue: mockActivatedRoute},
-      ]
+      ],
+      imports: [FormsModule]
     });
 
     fixture = TestBed.createComponent(PostDetailComponent);
   });
+
+  it('should render the post title in h2 template', (): void => {
+    mockPostService.getPost.and.returnValue(of({
+        id: 1,
+        userId: 1,
+        title: 'Title 1',
+        body: 'Body 1'
+      } as Post)
+    );
+
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.debugElement.query(By.css('h2')).nativeElement as HTMLElement;
+    expect(element.textContent).toBe(fixture.componentInstance.post.title);
+  })
 });
